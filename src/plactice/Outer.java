8ab72ee19cfd8752502a00a7713d28d045779a9e
kazukinakamuraKN <kajirikajiri@gmail.com>
2018-09-06 07:08:33 +0900

    error shuusei

--- src/plactice/Outer.java
+++ src/plactice/Outer.java
@@ -1,5 +1,17 @@
 package plactice;
 
 public class Outer {
+	String outerName = "outer";
 
+	public static void main(String[]args) {
+		Outer outer = new Outer();
+		Inner inner = outer.new Inner();
+		inner.outerAccess();
+		System.out.println(outer.outerName);
+	}
+	public class Inner{
+		void outerAccess() {
+			System.out.println(outerName);
+		}
+	}
 }
